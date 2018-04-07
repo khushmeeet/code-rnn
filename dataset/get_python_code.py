579e437ae8f7073a0183172b2377f578123f090f
khushmeet <khushmeetsingh1996@gmail.com>
2018-04-07 21:20:04 +0530

    complete code scraper and cleaner

--- dataset/get_python_code.py
+++ dataset/get_python_code.py
@@ -4,18 +4,21 @@ import os
 code_dirs = ['/Users/khushmeetsingh/anaconda/lib/python3.6',
              '/Users/khushmeetsingh/anaconda/lib/python3.6/site-packages']
 
+# Total files read  - 143993
 files_read = 0
 
-with open('python_code.txt', 'a', encoding='utf-8') as file:
+with open('python_code.txt', 'a', encoding='utf-8') as f:
     for code_dir in code_dirs:
+        print('Code Directory:', code_dir)
         for root, dirs, files in os.walk(code_dir):
             for file in files:
                 files_read += 1
-                if '.py' in file:
+                if file.split('.')[-1] == 'py':
                     try:
-                        with open(os.path.join(root, file), 'r') as code_file:
+                        with open(os.path.join(root, file), 'r', encoding='utf-8') as code_file:
                             content = code_file.read()
-                        file.write(content)
-                        file.write('\n\n')
+                        f.write(str(content))
+                        f.write('\n\n')
+                        print('Total Files Read:', files_read)
                     except Exception as e:
                         print(str(e))
---
+++ dataset/remove_comments_docstrings.py
@@ -0,0 +1,43 @@
+import datetime
+import sys
+import token
+import tokenize
+
+
+def do_file(fname):
+    source = open(fname)
+    mod = open(fname + "_processed", "w")
+
+    prev_toktype = token.INDENT
+    first_line = None
+    last_lineno = -1
+    last_col = 0
+
+    tokgen = tokenize.generate_tokens(source.readline)
+    for toktype, ttext, (slineno, scol), (elineno, ecol), ltext in tokgen:
+        if 0:   # Change to if 1 to see the tokens fly by.
+            print("%10s %-14s %-20r %r" % (
+                tokenize.tok_name.get(toktype, toktype),
+                "%d.%d-%d.%d" % (slineno, scol, elineno, ecol),
+                ttext, ltext
+            ))
+        if slineno > last_lineno:
+            last_col = 0
+        if scol > last_col:
+            mod.write(' ' * (scol - last_col))
+        if toktype == token.STRING and prev_toktype == token.INDENT:
+            mod.write('')
+        elif toktype == tokenize.COMMENT:
+            mod.write('')
+        else:
+            mod.write(ttext)
+        prev_toktype = toktype
+        last_col = ecol
+        last_lineno = elineno
+
+print('Working')
+start = datetime.datetime.second
+do_file('python_code.txt')
+end = datetime.datetime.second
+print('Done!')
+print('Time Taken:', end-start)
